5a3928f75a00be027f7d2d4ebf764e82fceeac93
jimconner <jamesdavidconner@gmail.com>
2021-01-16 11:08:03 -0600

    added variables, data types and math to js file

--- main.js
+++ main.js
@@ -1,1 +1,52 @@
-console.log("Greetings from main.js")
+// console.log("CONNECTED")
+
+// let firstName = 'Beyonce';
+// let lastName = 'Knowles';
+
+// console.log(firstName +  lastName);
+// console.log(`First Name: ${firstName} Last Name: ${lastName}`);
+
+// // **********START VARIABLES************
+// firstName = 'Jim';
+// lastName = 'Conner';
+
+// console.log(firstName +  lastName);
+// console.log(`First Name: ${firstName} Last Name: ${lastName}`);
+
+// const favoriteFood = 'pizza';
+// console.log(favoriteFood);
+
+// favoriteFood = 'enchiladas';
+// re-assigning this variable throws an error
+// console.log(favoriteFood);
+
+// **********END VARIABLES************
+
+
+// **********START DATA TYPES************
+// const dog = 'bark';
+// console.log(typeof dog);
+
+// const answer = 33;
+// const negative = -14;
+// console.log(typeof answer, typeof negative);
+
+// const iLoveJavascript = true;
+// console.log(typeof iLoveJavascript);
+
+// **********END DATA TYPES************
+
+// **********START MATH ************
+
+// console.log(Math.pow(2, 2));
+// console.log(Math.pow(3, 2));
+// console.log(Math.pow(3, 3));
+
+// console.log(Math.round(6.5)); // 7
+// console.log(Math.round(6.45)); // 6
+// console.log(Math.floor(6.9)); // 6
+// console.log(Math.ceil (6.00001)); // 7
+
+// const cat = 'kitty';
+// console.log(cat.length); // 5
+// **********END MATH ************
