dfc5ecc046df1eddfbba1efcb7f7d2c137b285e4
jimconner <jamesdavidconner@gmail.com>
2021-01-16 11:58:35 -0600

    added functions branch and practiced in main.js

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log("Greetings from main.js")
+// console.log("Greetings from main.js")
+// function sayHello(){
+
+//   console.log("Hello!")
+// }
+
+// const sayHello = function(){
+//   console.log("Hello!")
+// }
+
+// const sayHello = () => {
+//   console.log("Hello!")
+// }
+// sayHello();
+
+// ***********FUNCTIONS WITH ARGUMENTS **********
+
+// function logsHello(name){
+//   console.log(`Hello, ${name}!`);
+// }
+
+// logsHello('Jim');
+const sum = 55;
+
+const  addsTwoNumbers = (num1, num2) => {
+  const sum = num1 + num2;
+  console.log("console log inside the function", sum);
+  return sum;
+}
+console.log(sum);
+console.log(addsTwoNumbers(2,7));
+console.log(sum);
